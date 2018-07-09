ee179682d53f7abf313d7be2722f33b9aca13a7c
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-10 02:55:38 +0700

    Added database config

--- config/datastores.js
+++ config/datastores.js
@@ -48,9 +48,10 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
+    adapter: 'sails-mongo',
     // url: 'mysql://user:password@host:port/database',
-
+    host: 'localhost',
+    port: 27017
   },
 
 

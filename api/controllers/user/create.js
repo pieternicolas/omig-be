9ca7aaa70487e9c4863d0778ecf654c69bd38eaf
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-10 17:01:33 +0700

    Changed user creation to use a2

--- api/controllers/user/create.js
+++ api/controllers/user/create.js
@@ -1,14 +1,68 @@
-module.exports = async (req, res) => {
+module.exports = {
 
-  try {
 
-    const createdRecord = await User.create(req.body).fetch();
-    return res.json(createdRecord);
+  friendlyName: 'Create user',
 
-  } catch(err) {
 
-    return res.json(err.cause);
+  description: 'Insert new user to database',
 
-  };
+
+  inputs: {
+
+    username: {
+      type: 'string',
+      required: true,
+      unique: true,
+      maxLength: 50,
+      example: 'bananatictac12'
+    },
+
+    password: {
+      type: 'string',
+      required: true,
+      description: 'Not hashed version of password',
+      protect: true,
+      example: 'plainpassword'
+    },
+
+    fullName: {
+      type: 'string',
+      description: 'Full name of current user',
+      example: 'Nicolas Pieter'
+    }
+
+  },
+
+
+  exits: {
+
+    badRequest: {
+      description: 'If one or more fields are missing or does not adhere to the rules above',
+      responseType: 'badRequest'
+    },
+
+    duplicateUsername: {
+      description: 'If the username already exists on the database',
+      responseType: 'duplicate'
+    }
+
+  },
+
+
+  fn: async (inputs, exits) => {
+
+    const createdRecord = await User.create(inputs)
+      .intercept('E_UNIQUE', (err)=> {
+        return exits.duplicateUsername(err);
+      })
+      .intercept('UsageError', (err)=> {
+        return exits.badRequest(err);
+      })
+      .fetch();
+
+    return exits.success(createdRecord);
+
+  }
+
 
 };

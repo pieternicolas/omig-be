10b29cda018f22233224712eb5ba63144661c9c1
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-11 22:01:15 +0700

    Added account model

---
+++ api/models/Account.js
@@ -0,0 +1,53 @@
+/**
+ * Account.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    username: {
+      type: 'string',
+      required: true,
+      unique: true,
+      maxLength: 50,
+      description: 'Instagram account username',
+      example: 'bananatictac12'
+    },
+
+    name: {
+      type: 'string',
+      maxLength: 100,
+      description: 'Instagram account name (not required, only for frontend purposes)',
+      example: 'Account jual kacang'
+    },
+
+    description: {
+      type: 'string',
+      description: 'Account description for frontend purposes only',
+      example: 'Ini account yg buat jualan kacang'
+    },
+
+    userID: {
+      model: 'user'
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+

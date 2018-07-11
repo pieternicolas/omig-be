f808a26f42b5ff6bb9a7ab756a268392d5040b2c
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-11 22:01:29 +0700

    Added account related actions

---
+++ api/controllers/account/create.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Create account',
+
+
+  description: 'Create an instagram related account on the user.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};

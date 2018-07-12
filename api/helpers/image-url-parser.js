7cfc35450907ea9ec54d3cbc85031058398e0a9a
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-13 03:20:36 +0700

    Added image url parser file

---
+++ api/helpers/image-url-parser.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+
+  friendlyName: 'Image url parser',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    // All done.
+    return exits.success();
+
+  }
+
+
+};
+
